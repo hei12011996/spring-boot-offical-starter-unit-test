2c54324aecb398d7001aafca6e70855fb9a97244
Antony Chan <antony.chan@oocl.com>
2018-11-29 00:20:52 +0800

    feat: add UserController

---
+++ src/main/java/com/oocl/web/sampleWebApp/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.oocl.web.sampleWebApp.controller;
+
+import com.oocl.web.sampleWebApp.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private Long userId = 0L;
+
+    @PostMapping(produces = {"application/json"})
+    public ResponseEntity<String> create(@RequestBody User user){
+        return null;
+    }
+
+}
